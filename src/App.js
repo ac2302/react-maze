01c14c377f0bbb76505181f86043eba5d10c824e
Aryan Chavan <aryan@aryansdomain.com>
2021-06-29 15:44:44 +0530

    redo evverything from scratch

--- src/App.js
+++ src/App.js
@@ -1,26 +1,9 @@
 import { react, useState } from "react";
-import Grid from "./Grid";
 
 function App() {
-	const [width, setWidth] = useState(10);
-	const [height, setHeight] = useState(5);
-	const [cells, setCells] = useState(
-		Array(height)
-			.fill(1)
-			.map(() =>
-				Array(width)
-					.fill(1)
-					.map(() => {
-						return 0;
-					})
-			)
-	);
-
-	let [cellSize, setCellSize] = useState(10);
-
 	return (
 		<>
-			<Grid maze={cells} cellSize={cellSize} />
+			<h1>I'm redoing everything. FML</h1>
 		</>
 	);
 }
